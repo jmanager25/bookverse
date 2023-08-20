42dc360b62232a46e23789da4a1ac78c9e49b5d7
jmanager25 <jmanager25@hotmail.com>
2023-08-20 12:26:28 +0000

    Update book's list view

--- books/views.py
+++ books/views.py
@@ -1,7 +1,13 @@
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+class BookList(APIView):
+    """
+    Retrieves the book's list
+    """
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
