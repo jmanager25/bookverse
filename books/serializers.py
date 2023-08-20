fd687fda3f78f91685a09cdeabdffd4350ed5121
jmanager25 <jmanager25@hotmail.com>
2023-08-20 13:18:43 +0000

    Add update, delete and permissions to book detail

--- books/serializers.py
+++ books/serializers.py
@@ -3,10 +3,15 @@ from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
     class Meta:
         model = Book
         fields = [
             'id', 'owner', 'title', 'summary', 'author', 'genre', 'cover_image',
-            'average_rating', 'created_at', 'updated_at'
+            'average_rating', 'created_at', 'updated_at', 'is_owner'
         ]
--- books/views.py
+++ books/views.py
@@ -3,6 +3,10 @@ from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer
 from django.http import Http404
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticatedOrReadOnly
+from bookverse_api.permissions import IsOwnerOrReadOnly
+
 
 class BookList(APIView):
     """
@@ -10,7 +14,8 @@ class BookList(APIView):
     """
     def get(self, request):
         books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
+        serializer = BookSerializer(
+            books, many=True, context={'request': request})
         return Response(serializer.data)
 
 class BookDetail(APIView):
@@ -18,6 +23,9 @@ class BookDetail(APIView):
     Retrieves, updates and deletes books. Also manages
     permissions.
     """
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
     def get_book(self, pk):
         try:
             book = Book.objects.get(pk=pk)
@@ -27,5 +35,19 @@ class BookDetail(APIView):
     
     def get(self, request, pk):
         book = self.get_book(pk)
-        serializer = BookSerializer(book)
+        serializer = BookSerializer(book, context={'request': request})
         return Response(serializer.data)
+    
+    def put(self, request, pk):
+        book = self.get_book(pk)
+        serializer = BookSerializer(
+            book, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        book = self.get_book(pk)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
