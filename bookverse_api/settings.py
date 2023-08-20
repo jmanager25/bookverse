299f7f796e19760ee70bc735513a135e06d255cf
jmanager25 <jmanager25@hotmail.com>
2023-08-20 16:16:36 +0000

    Prepare react to connect to the django api

--- bookverse_api/settings.py
+++ bookverse_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists('env.py'):
     import env
@@ -32,13 +33,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e$+a84g^xcmd-g*f%l9q@5*qpewgwv@%1o^j)r(5q$g1b(vo9f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = [
-    '8000-jmanager25-bookverse-2qp4v80fjjp.ws-eu104.gitpod.io',
-    'https://3000-jmanager25-bookverse-2qp4v80fjjp.ws-eu104.gitpod.io/',
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    os.environ.get('CLIENT_ORIGIN')
+]
 
 # Application definition
 
@@ -90,12 +94,17 @@ WSGI_APPLICATION = 'bookverse_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
 
 # Password validation
