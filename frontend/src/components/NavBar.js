c8de1e38d963763d6c6fc8a48bd7521f2c053846
jmanager25 <jmanager25@hotmail.com>
2023-08-22 14:46:46 +0000

    Add css files and icons to navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import {Form, Navbar, Container, Button, Nav} from "react-bootstrap";
+import styles from "../styles/NavBar.module.css"
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top" className="bg-body-tertiary">
+    <Navbar expand="md" fixed="top" className={styles.NavBar}>
         <Container fluid>
           <Navbar.Brand href="#">BookVerse</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">My Books</Nav.Link>
-            <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
+            <Nav className="mx-auto">
+                <Nav.Link href="#action1"><i class="fa-solid fa-house"></i>Home</Nav.Link>
+                <Nav.Link href="#action2"><i class="fa-solid fa-book"></i>My Books</Nav.Link>
+                <Form className="d-flex">
+                <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+                <i class="fa-solid fa-magnifying-glass"></i>
+                </Form>
+            </Nav>
             <Nav
               className="ml-auto"
             >
-              <Nav.Link href="#action1">Sign in</Nav.Link>
-              <Nav.Link href="#action2">Sign up</Nav.Link>
+              <Nav.Link href="#action1"><i class="fa-solid fa-right-to-bracket"></i>Sign in</Nav.Link>
+              <Nav.Link href="#action2"><i class="fa-solid fa-user-plus"></i>Sign up</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
