4ce0447ee028475d72401de5b596736d35ba6d4b
jmanager25 <jmanager25@hotmail.com>
2023-09-09 08:21:15 +0000

    Style book component

--- frontend/src/components/Books.js
+++ frontend/src/components/Books.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import styles from '../styles/Books.module.css';
 import buttonstyles from '../styles/Button.module.css'
-import {Container, Row, Button, Card} from "react-bootstrap";
+import {Container, Row, Button, Card, Col} from "react-bootstrap";
 import { axiosReq } from '../api/axiosDefaults';
 
 
@@ -24,9 +24,13 @@ const Books = () => {
         <Row xs={1} md={2} lg={3}>
             {books.map((book) => (
                 <Card key={book.id} className={styles.Card}>
-                    <Card.Title>{book.title}</Card.Title>
-                    <Card.Img variant='top' src={book.cover_image} alt={book.title} />
-                    <Button className={buttonstyles.Button}>Save Book</Button>
+                    <Card.Img variant='top' src={book.cover_image} alt={book.title} className={styles.CardImage} />
+                    <Card.Body>
+                        <Card.Title className='text-center'>{book.title}</Card.Title>
+                        <div className='text-center'>
+                            <Button className={buttonstyles.Button}>Save Book</Button>
+                        </div>
+                    </Card.Body>
                 </Card>
             ))}
         </Row>
